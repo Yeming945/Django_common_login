7aefe971e5f6ebf8138e2046fc1a32aeb7132c22
Flaming <yeming945@qq.com>
2019-08-29 15:51:30 +0800

    part6登录视图

--- login/views.py
+++ login/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import redirect
+from. import models
 
 
 def index(request):
@@ -10,7 +11,24 @@ def index(request):
 
 
 def login(request):
-    pass
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        message = '请检查填写的内容'
+        if username.strip() and password:  # 确保用户名密码不为空
+            try:
+                user = models.User.objects.get(name=username)
+            except:
+                message = '用户不存在!'
+                return render(request, 'login/login.html', {'message': message})
+            if user.password == password:
+                print(username, password)
+                return redirect('/index/')
+            else:
+                message = '密码不正确!'
+                return render(request, 'login/login.html', {'message': message})
+        else:
+            return render(request, 'login/login.html', {'message':message})
     return render(request, 'login/login.html')
 
 
@@ -18,7 +36,10 @@ def register(request):
     pass
     return render(request, 'login/register.html')
 
+
 """ 用户登出后重定向 """
+
+
 def logout(request):
     pass
     return redirect('/login/')
